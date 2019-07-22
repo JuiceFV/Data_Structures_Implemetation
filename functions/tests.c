fe7f785e2bfa3bd3365cf05806cab2ba2659bd5b
JuiceFV <alexcar19622000@gmail.com>
2019-07-22 22:37:13 +0300

    Tests for queue added

--- functions/tests.c
+++ functions/tests.c
@@ -1,7 +1,6 @@
 #include "../headers/tests.h"
 
 Bool test_stack_init(stack* st, int _size) {
-  stack_init(st, _size);
   int result = 0;
   cell* temp = st->begin;
   while (temp != NULL) {
@@ -32,4 +31,47 @@ Bool test_stack_pop(stack* st) {
     return False;
 }
 
+Bool test_stack_get_el(stack* st, int index, int el) {
+  if (stack_get_el(st, index) == el) {
+    return True;
+  } else {
+    return False;
+  }
+}
 
+Bool test_queue_init(queue* qu, int _size) {
+  int result = 0;
+  q_cell* temp = qu->begin;
+  while (temp != NULL) {
+    result++;
+    temp->prev = temp;
+    temp = temp->next;
+  }
+  if (result == _size)
+    return True;
+  else
+    return False;
+}
+Bool test_queue_push(queue* qu, int el) {
+  queue_push(qu, el);
+  if (qu->end->key == el)
+    return True;
+  else
+    return False;
+}
+Bool test_queue_pop(queue* qu) {
+  int before = qu->begin->key;
+  queue_pop(qu);
+  int after = qu->begin->key;
+  if (before != after)
+    return True;
+  else
+    return False;
+}
+Bool test_queue_get_el(queue* qu, int index, int el) {
+  if (queue_get_el(qu, index) == el) {
+    return True;
+  } else {
+    return False;
+  }
+}
--- headers/tests.h
+++ headers/tests.h
@@ -2,10 +2,16 @@
 #define _TESTS
 #include <stdio.h>
 #include "../headers/stack.h"
+#include"../headers/queue.h"
 
 typedef enum { True, False } Bool;
 
 Bool test_stack_init(stack* st, int _size);
 Bool test_stack_push(stack* st, int el);
 Bool test_stack_pop(stack* st);
+Bool test_stack_get_el(stack* st, int index, int el);
+Bool test_queue_init(queue* qu, int _size);
+Bool test_queue_push(queue* qu, int el);
+Bool test_queue_pop(queue* qu);
+Bool test_queue_get_el(queue* qu, int index, int el);
 #endif _TESTS
