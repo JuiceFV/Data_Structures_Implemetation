371966e5cf18c5beb52069d16d0c156792532bf5
JuiceFV <alexcar19622000@gmail.com>
2020-01-07 21:41:40 +0300

    write_logs removed

--- sources/common.c
+++ sources/common.c
@@ -3,16 +3,8 @@
 void write_error_log(const char* error_message, int line,
                             const char* file, const char* specific_dir) {
   FILE* fout;
-  fout = fopen("error_logs.txt", "at");
+  fout = fopen("error_logs.txt", "w");
   fprintf(fout, "Assertion error line %d, file(%s) %s\n%s\n", line, file,
           specific_dir, error_message);
-  fclose(fout);
-}
-
-void write_log(const char* message, const char* function) {
-  FILE* fout;
-  fout = fopen("logs.txt", "at");
-  fprintf(fout, "Function %s has successfully completed.\nDetails: %s\n\n",
-          function, message);
   fclose(fout);
 }
