2bb73f6fd7ca97d2ad038737b9d6e7f845a4bdd9
JuiceFV <alexcar19622000@gmail.com>
2020-01-16 11:10:17 +0300

    main.c modififed

--- main.c
+++ main.c
@@ -1,9 +1,21 @@
 #include "./includes/stack.h"
 #include "./includes/queue.h"
-
+typedef char* string;
 int main() {
-    queue(int)* a = NULL;;
-    queue_constructor(int, a);
+    queue(string)* a = NULL;
+    queue_constructor(string, a);
+    queue_enqueue(a, "Daun");
+    queue_enqueue(a, "Pizdec");
+    queue_enqueue(a, "Ahahahaha");
+    queue_enqueue(a, "Dolboeb");
+    queue_enqueue(a, "Sanya");
+    printf("%s ", queue_dequeue(a));
+    printf("%s ", queue_dequeue(a));
+    printf("%s ", queue_dequeue(a));
+    printf("%s ", queue_dequeue(a));
+    printf("%s ", queue_dequeue(a));
+    queue_enqueue(a, "Sanya");
     queue_destructor(a);
+    _CrtDumpMemoryLeaks();
   return (0);
 }
