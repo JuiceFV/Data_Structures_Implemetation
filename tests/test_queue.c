8d5b8d39de6477c3cf5294cf84e8b1dbca9f52f1
JuiceFV <aleksandr.juicefv@gmail.com>
2020-06-26 17:13:02 +0300

    added some tests for queue

--- tests/test_queue.c
+++ tests/test_queue.c
@@ -2,13 +2,97 @@
 #include <stddef.h>
 #include <setjmp.h>
 #include <cmocka.h>
-/* A test case that does nothing and succeeds. */
-static void null_test_success(void** state) {
-    (void)state; /* unused */
+
+#include "premain.h"
+#include "queue.h"
+
+static void queue_correct_constructor_behavior_test() {
+  queue(int)* a = NULL;
+
+  queue_constructor(int, a);
+  if (return_val_from_macro == 0) {
+    assert_true(a != NULL);
+    assert_true(a->begin == NULL);
+    assert_true(a->end == NULL);
+    assert_true(a->size == 0);
+  } else {
+    printf("Something went wrong in queue_correct_constructor_behavior_test");
+    queue_destructor(a);
+    exit(getchar());
+  }
+  queue_destructor(a);
 }
+
+#if defined(_MSC_VER)
+    static void queue_incorrect_constructor_behavior_test() {
+      queue(int)* a = NULL;
+      queue_constructor(int, a);
+      if (return_val_from_macro == 0) {
+        queue_constructor(int, a);
+        if (return_val_from_macro == -1) {
+          FILE* file = fopen("error_logs.txt", "r");
+          char buff[255];
+          char* line = fgets(buff, 255, file);
+          fclose(file);
+          assert_true(line != NULL);
+        } else {
+          printf(
+              "Something went wrong in queue_incorrect_constructor_behavior_test");
+          queue_destructor(a);
+          exit(getchar());
+        }
+      } else {
+        printf("Something went wrong in queue_incorrect_constructor_behavior_test");
+        queue_destructor(a);
+        exit(getchar());
+      }
+      queue_destructor(a);
+    }
+#endif
+
+static void queue_correct_destructor_behavior_test() {
+  queue(int)* a = NULL;
+  queue_constructor(int, a);
+  if (return_val_from_macro == 0) {
+    assert_true(a != NULL);
+    queue_destructor(a);
+    if (return_val_from_macro == 0) {
+      assert_int_not_equal(a->size, 0);
+    } else {
+      printf("Something went wrong in queue_correct_destructor_behavior_test");
+      queue_destructor(a);
+      exit(getchar());
+    }
+  } else {
+    printf("Something went wrong in queue_correct_destructor_behavior_test");
+    queue_destructor(a);
+    exit(getchar());
+  }
+}
+
+static void queue_incorrect_destructor_behavior_test() {
+  queue(int)* a = NULL;
+  queue_destructor(a);
+  if (return_val_from_macro == -1) {
+    FILE* file = fopen("error_logs.txt", "r");
+    char buff[255];
+    char* line = fgets(buff, 255, file);
+    fclose(file);
+    assert_true(line != NULL);
+  } else {
+    printf("Something went wrong in queue_incorrect_destructor_behavior_test");
+    exit(getchar());
+  }
+}
+
 int main(void) {
-    const struct CMUnitTest tests[] = {
-        cmocka_unit_test(null_test_success),
-    };
-    return cmocka_run_group_tests(tests, NULL, NULL);
+  const struct CMUnitTest tests[] = {
+      cmocka_unit_test(queue_correct_constructor_behavior_test),
+#if defined(_MSC_VER)
+      cmocka_unit_test(queue_incorrect_constructor_behavior_test),
+#endif
+      cmocka_unit_test(queue_correct_destructor_behavior_test),
+      cmocka_unit_test(queue_incorrect_destructor_behavior_test),
+  };
+  return cmocka_run_group_tests(tests, NULL, NULL);
 }
