2366ed18a365908651c290311a5d7d5aacb398bd
JuiceFV <aleksandr.juicefv@gmail.com>
2020-07-18 12:26:26 +0300

    eee

--- tests/test_queue.c
+++ tests/test_queue.c
@@ -133,36 +133,6 @@ static void queue_correct_dequeue_behavior_test(void** state) {
   }
 }
 
-#if defined(_MSC_VER)
-static void queue_incorrect_dequeue_behavior_test() {
-  queue(int)* a = NULL;
-  queue_constructor(int, a);
-#elif defined(__GNUC__)
-static void queue_incorrect_dequeue_behavior_test(void** state) {
-  queue(int)* a = queue_constructor(int);
-#endif
-  if (return_val_from_macro != NULL) {
-    queue_dequeue(a);
-    if (return_val_from_macro == NULL) {
-      FILE* file = fopen("error_logs.txt", "r");
-      char buff[255];
-      char* line = fgets(buff, 255, file);
-      fclose(file);
-      assert_true(line != NULL);
-      file = fopen("error_logs.txt", "w");
-      fprintf(file, "");
-      fclose(file);
-    } else {
-      printf("Something went wrong in queue_correct_dequeue_behavior_test");
-      queue_destructor(a);
-      exit(getchar());
-    }
-  } else {
-    printf("Something went wrong in queue_correct_dequeue_behavior_test");
-    queue_destructor(a);
-    exit(getchar());
-  }
-}
 
 int main(void) {
   const struct CMUnitTest tests[] = {
@@ -173,7 +143,6 @@ int main(void) {
     cmocka_unit_test(queue_correct_destructor_behavior_test),
     cmocka_unit_test(queue_incorrect_destructor_behavior_test),
     cmocka_unit_test(queue_correct_dequeue_behavior_test),
-    cmocka_unit_test(queue_incorrect_dequeue_behavior_test),
   };
   return cmocka_run_group_tests(tests, NULL, NULL);
 }
